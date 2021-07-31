c82b5c5e2dbfccadc718c0c6d0e5c68f0131b924
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-08-01 02:26:39 +0530

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <Container />
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Container from '@/components/Container.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Container,
+  },
+};
+</script>
