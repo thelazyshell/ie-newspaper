033933e1213ed5ed6061e949a60b47fc532228d7
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-08-01 04:22:16 +0530

    first build

---
+++ docs/precache-manifest.f5f543ffce7cb64a3bbe7e9d49add38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c3c0759c305b432fa2",
+    "url": "/ie-newspaper/css/chunk-vendors.971ba6a1.css"
+  },
+  {
+    "revision": "ceb4a88f44e24b44a2f10e35dbe81566",
+    "url": "/ie-newspaper/index.html"
+  },
+  {
+    "revision": "bbb0800530aa2f2b9509",
+    "url": "/ie-newspaper/js/app.c37432bf.js"
+  },
+  {
+    "revision": "99c3c0759c305b432fa2",
+    "url": "/ie-newspaper/js/chunk-vendors.66e9768e.js"
+  },
+  {
+    "revision": "b494208fced63bcd17b57bba917cfd7c",
+    "url": "/ie-newspaper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ie-newspaper/robots.txt"
+  }
+]);
