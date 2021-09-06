e4339c72e1a8004418d431a37a77d0a6a90fb696
AadityaJain-Dev <AadityaJain-Dev@users.noreply.github.com>
2021-09-06 23:19:13 +0000

    Deploying to publish from @ thelazyshell/ie-newspaper@b6cab541fa4d01c85d2d1f24bf6e1f12a5800fd8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ie-newspaper/precache-manifest.59f06532d55b1e4cc3ed0a54e85f4b2a.js"
+  "/ie-newspaper/precache-manifest.621ec4dfc1aa59a05c45fdcc92f6e759.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ie"});
